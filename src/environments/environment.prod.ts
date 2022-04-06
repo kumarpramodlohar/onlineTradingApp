f2aa6d3e5c4ed05c1c4d4e28e5e809ebe5af477a
kumarpramodlohar <kumarpramod.asn@gmail.com>
2022-04-06 22:21:42 +0530

    after apply responsive

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'http://ec2-52-38-131-69.us-west-2.compute.amazonaws.com:8080',
+  API_URL: 'http://ec2-34-209-93-121.us-west-2.compute.amazonaws.com:8080',
 };
+
+
